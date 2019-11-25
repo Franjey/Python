cf3fd9166d13415429cf7ab66ec071d81cc17781
FDJ <franciscojurado2@gmail.com>
2019-11-25 11:18:23 -0300

    Update InstaPost_Spanish.py

--- Instagram/InstaPost_Spanish.py
+++ Instagram/InstaPost_Spanish.py
@@ -20,7 +20,7 @@ sleep(3)
 driver.find_element_by_xpath("//button[text()='Iniciar sesión']").click()
 sleep(3)
 driver.find_element_by_name("username").send_keys("insta_dog123@fastmail.com")
-driver.find_element_by_name("password").send_keys("medallas")
+driver.find_element_by_name("password").send_keys("password")
 driver.find_element_by_xpath("//button[@type='submit']").click()
 sleep(5)
 
